a34eff33661c3b1d05f67b76fde581e5b67095f7
Heath McGowan <heathmcgowan@Heaths-MacBook-Pro.local>
2020-08-20 23:03:50 +1200

    Updates

---
+++ precache-manifest.9ca44ada5f59e8d5649d428cef75f433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8b5d812fa19a21e60ec4a2da099ddb",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a763a54b0d39a702768e",
+    "url": "/todo-app/static/css/main.5cb3bddf.chunk.css"
+  },
+  {
+    "revision": "6453e8d1ae0740857d52",
+    "url": "/todo-app/static/js/2.a0a94971.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.a0a94971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a763a54b0d39a702768e",
+    "url": "/todo-app/static/js/main.63a9f186.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "ee6114b173ca4f6dd5a45258011f8bbf",
+    "url": "/todo-app/static/media/temp-logo.ee6114b1.png"
+  }
+]);
